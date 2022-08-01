c527c64c15d8eb81bf4f5893ddfb4c207bee8173
sven <sven@wehi.cc>
2022-08-02 00:38:04 +0800

    @deploy

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	_ "github.com/go-redis/redis"
+	"net/http"
 
 	"fmt"
 )
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello world")
+}
+
 func main() {
-	fmt.Println("Hello rainbond-go")
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8899", nil)
 }
